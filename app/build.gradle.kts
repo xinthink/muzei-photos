eedc57169dd1630e36a6f0a3dc3a92689aaf640a
Think Wu <yingxinwu.g@gmail.com>
2019-09-05 19:34:45 +0800

    bump app version

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,8 +12,8 @@ android {
         applicationId = "com.xinthink.muzei.photos"
         minSdkVersion(V.minSdkVersion)
         targetSdkVersion(V.targetSdkVersion)
-        versionCode = 4
-        versionName = "1.2.0"
+        versionCode = 5
+        versionName = "1.3.0"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
             useSupportLibrary = true
