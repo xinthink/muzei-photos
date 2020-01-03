643aadeba2c9cce792a269c83ce717f92124a1c0
Think Wu <yingxinwu.g@gmail.com>
2020-01-03 12:27:48 +0800

    upgrade android plugin & dependencies

--- buildSrc/src/main/java/deps.kt
+++ buildSrc/src/main/java/deps.kt
@@ -10,11 +10,11 @@ object V {
     const val compileSdkVersion = 28
     const val targetSdkVersion = 28
     const val minSdkVersion = 19
-    const val androidPluginVersion = "3.5.0"
+    const val androidPluginVersion = "3.5.3"
 
     const val kotlinVersion = "1.3.50"
     const val ktStdlib = "stdlib-jdk7"
-    const val coroutinesVersion = "1.3.0-RC2"
+    const val coroutinesVersion = "1.3.3"
     const val ankoVersion = "0.10.8"
 
     // Firebase
@@ -23,22 +23,22 @@ object V {
     const val firebaseAnalyticsVersion = "17.2.0"
     const val crashlyticsVersion = "2.10.1"
 
-    const val appCompatVersion = "1.1.0-rc01"
+    const val appCompatVersion = "1.1.0"
     const val browserVersion = "1.0.0"
     const val constraintLayoutVersion = "1.1.3"
-    const val coreVersion = "1.2.0-alpha03"
-    const val archVersion = "2.0.0-rc01"
-    const val lifecycleVersion = "2.2.0-alpha03"
-    const val navVersion = "1.0.0-rc02"
-    const val preferenceVersion = "1.1.0-alpha03"
-    const val materialVersion = "1.1.0-alpha09"
+    const val coreVersion = "1.2.0-rc01"
+    const val archVersion = "2.1.0"
+    const val lifecycleVersion = "2.2.0-rc03"
+    const val navVersion = "2.1.0"
+    const val preferenceVersion = "1.1.0"
+    const val materialVersion = "1.1.0-beta02"
     const val exifInterfaceVersion = "1.0.0"
-    const val retrofitVersion = "2.6.1"
-    const val okhttpVersion = "3.12.1"
+    const val retrofitVersion = "2.6.3"
+    const val okhttpVersion = "3.14.4"
     const val picassoVersion = "2.71828"
     const val picassoTransformationsVersion = "2.2.1"
-    const val workManagerVersion = "1.0.0"
-    const val muzeiApiVersion = "3.1.0"
+    const val workManagerVersion = "2.2.0"
+    const val muzeiApiVersion = "3.2.0"
     const val gmsAuthVersion = "16.0.1"
 
     const val ktlintVersion = "0.34.2"
@@ -90,7 +90,7 @@ val DependencyHandler.navigationFragment get() = "android.arch.navigation:naviga
 val DependencyHandler.material get() = "com.google.android.material:material:${V.materialVersion}"
 val DependencyHandler.muzeiApi get() = "com.google.android.apps.muzei:muzei-api:${V.muzeiApiVersion}"
 val DependencyHandler.gmsAuth get() = "com.google.android.gms:play-services-auth:${V.gmsAuthVersion}"
-val DependencyHandler.workRuntime get() = "android.arch.work:work-runtime-ktx:${V.workManagerVersion}"
+val DependencyHandler.workRuntime get() = "androidx.work:work-runtime-ktx:${V.workManagerVersion}"
 val DependencyHandler.retrofit get() = "com.squareup.retrofit2:retrofit:${V.retrofitVersion}"
 val DependencyHandler.retrofitMoshi get() = "com.squareup.retrofit2:converter-moshi:${V.retrofitVersion}"
 val DependencyHandler.retrofitRx get() = "com.squareup.retrofit2:adapter-rxjava2:${V.retrofitVersion}"
