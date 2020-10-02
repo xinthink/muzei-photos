36611d92c6d5318219dbba44901a9ca957597073
Think Wu <yingxinwu.g@gmail.com>
2020-10-02 11:44:57 +0800

    fix lint alerts

--- muzeiPhotos/src/main/java/com/xinthink/muzei/photos/firebase.kt
+++ muzeiPhotos/src/main/java/com/xinthink/muzei/photos/firebase.kt
@@ -6,7 +6,7 @@ import com.google.firebase.analytics.FirebaseAnalytics
 import com.google.firebase.analytics.ktx.analytics
 import com.google.firebase.ktx.Firebase
 
-/// Reports a firebase event with normalized key/value.
+/** Reports a firebase event with normalized key/value. */
 fun logEvent(@Size(min = 1, max = 40) name: String, vararg params: Pair<String, Any?>) {
     val theParams = mutableListOf<Pair<String, Any?>>()
     for ((k, v) in params) {
@@ -18,7 +18,7 @@ fun logEvent(@Size(min = 1, max = 40) name: String, vararg params: Pair<String, 
     Firebase.analytics.logEvent(name, bundleOf(*theParams.toTypedArray()))
 }
 
-/// Reports a firebase event with normalized key/value.
+/** Reports a firebase event with normalized key/value. */
 fun FirebaseAnalytics.logEvent(@Size(min = 1, max = 40) name: String, vararg params: Pair<String, Any?>) {
     val theParams = mutableListOf<Pair<String, Any?>>()
     for ((k, v) in params) {
@@ -29,7 +29,7 @@ fun FirebaseAnalytics.logEvent(@Size(min = 1, max = 40) name: String, vararg par
     logEvent(name, bundleOf(*theParams.toTypedArray()))
 }
 
-/// Set normalized user properties
+/** Set normalized user properties */
 fun FirebaseAnalytics.setUserProperties(vararg properties: Pair<String, String>) {
     for ((k, v) in properties) {
         val name = if (k.length > 24) k.substring(0, 24) else k
